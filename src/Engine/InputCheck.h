ea8d2eb5e08f4a3e07902e38068408a0062f5e0e
Gonzalo Alvarez <gz1@ornl.gov>
2021-11-17 18:48:13 -0500

    InputCheck added; input0.ain added

---
+++ src/Engine/InputCheck.h
@@ -0,0 +1,45 @@
+#ifndef INPUTCHECK_H
+#define INPUTCHECK_H
+#include "PsimagLite.h"
+
+namespace Gep {
+
+class InputCheck {
+
+public:
+
+	PsimagLite::String import() const
+	{
+		PsimagLite::String str;
+		str += "integer HeadSize;\n";
+		str += "integer Population;\n";
+		str += "integer Generations;\n";
+		str += "integer NumberOfBits;\n";
+
+		return str;
+	}
+
+	// POD inputs
+	bool checkSimpleLabel(const PsimagLite::String& label,
+	                      SizeType line) const
+	{
+		return true;
+	}
+
+	// SolverOptions= check
+	void check(const PsimagLite::String& label,
+	           const PsimagLite::String& val,
+	           SizeType)
+	{}
+
+	bool check(const PsimagLite::String& label,
+	           const PsimagLite::Vector<PsimagLite::String>::Type& vec,
+	           SizeType line) const
+	{
+		return true;
+	}
+
+
+};
+}
+#endif // INPUTCHECK_H
