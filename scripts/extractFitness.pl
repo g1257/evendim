5ed7feb64c6cc5eeea09f5a2c7b096404a22a23e
Gonzalo Alvarez <gz1@ornl.gov>
2022-09-01 17:28:00 -0400

    extract fitness script

--- scripts/extractFitness.pl
+++ scripts/extractFitness.pl
@@ -9,13 +9,29 @@ my ($file) = @ARGV;
 my $flag = 1;
 my $counter = 0;
 open(FILE, "<", "$file") or die "$0: Cannot open $file : $!\n";
-
+my $lastFit = 0;
+my $fit;
+my $maxDiff = 0;
 while (<FILE>) {
-	if ($flag and /fitness ([^ ]+) /) {
-		my $value = -$1;
-		print "$counter $value\n";
+	if (/fit ([^ ]+) /) {
+		my $b = defined($fit) ? 1 : 0;
+		$b = defined($lastFit) ? $b : 0;
+		my $tmp = ($b) ? abs($lastFit - $fit) : 0;
+		$maxDiff = $tmp if ($tmp > $maxDiff);
+
+		$lastFit = $fit;
+		$fit = $1;
+
+	}
+
+	if ($flag and /fit ([^ ]+) /) {
+		die "$0: Value $1 not equal fit $fit\n" if (defined($fit) and $1 != $fit);
+
+		my $value = 3-$1;
+		print "$counter $value $maxDiff\n";
 		++$counter;
-		#$flag = 0;
+		$flag = 0;
+		$maxDiff = 0;
 	}
 
 	last if ($counter == 3000);
