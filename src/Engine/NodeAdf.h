9e6730bc62936bde23670b81b36ddacf51ee6aa7
Gonzalo Alvarez <gz1@ornl.gov>
2022-10-26 16:47:23 -0400

    NodeAdf.h and NodeDc.h added

---
+++ src/Engine/NodeAdf.h
@@ -0,0 +1,62 @@
+/*
+Copyright (c) 2017-2022, UT-Battelle, LLC
+
+evendim, Version 0.
+
+This file is part of evendim.
+evendim is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+evendim is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with evendim. If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef NODEADF_H
+#define NODEADF_H
+#include "AST/Node.h"
+
+namespace Gep {
+
+template<typename VectorValueType, typename AnglesType_ = int>
+class NodeAdf : public PsimagLite::Node<VectorValueType, AnglesType_> {
+
+	typedef typename VectorValueType::value_type ValueType;
+
+public:
+
+	NodeAdf(SizeType i,ValueType input_)
+	    : char_(i+48),strOneChar_(" ")
+	{
+		strOneChar_[0] = char_;
+	}
+
+	NodeAdf* clone() const
+	{
+		return new NodeAdf(*this);
+	}
+
+	virtual PsimagLite::String code() const { return strOneChar_; }
+
+	virtual SizeType arity() const { return 0; }
+
+	virtual ValueType exec(const VectorValueType& v) const
+	{
+		assert(v.size() == 0);
+		return value_;
+	}
+
+	virtual void set(const ValueType& x) const { value_ = x; }
+
+private:
+
+	char char_;
+	PsimagLite::String strOneChar_;
+	mutable ValueType value_;
+
+}; // class NodeAdf
+}
+#endif // NODEADF_H
---
+++ src/Engine/NodeDc.h
@@ -0,0 +1,58 @@
+/*
+Copyright (c) 2017-2022, UT-Battelle, LLC
+
+evendim, Version 0.
+
+This file is part of evendim.
+evendim is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+evendim is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with evendim. If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef NODEDC_H
+#define NODEDC_H
+#include "AST/Node.h"
+
+namespace Gep {
+
+template<typename VectorValueType>
+class NodeDc : public PsimagLite::Node<VectorValueType> {
+
+	typedef typename VectorValueType::value_type ValueType;
+
+public:
+
+	NodeDc* clone() const
+	{
+		return new NodeDc(*this);
+	}
+
+	virtual PsimagLite::String code() const { return "?"; }
+
+	virtual SizeType arity() const { return 0; }
+
+	virtual void setDcValue(const ValueType& value) const
+	{
+		value_ = value;
+	}
+
+	virtual ValueType exec(const VectorValueType& v) const
+	{
+		assert(v.size() == 0);
+		return value_;
+	}
+
+private:
+
+	mutable ValueType value_;
+
+}; // class NodeDc
+
+}
+#endif // NODEDC_H
