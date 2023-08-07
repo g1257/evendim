e83b02a243d526a5fec38af0614619a0c139196e
Gonzalo Alvarez <gz1@ornl.gov>
2023-08-07 17:56:25 -0400

    XACC plugin

---
+++ src/xacc/QuantumGEP_Plugin.cpp
@@ -0,0 +1,49 @@
+#include "QuantumGEP_Plugin.hh"
+
+#include "PauliOperator.hpp"
+#include <iomanip>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+
+using namespace xacc;
+using namespace xacc::quantum;
+
+namespace xacc {
+namespace algorithm {
+bool QuantumGEP::initialize(const HeterogeneousMap &parameters) {
+  /** Checks for the required parameters and other optional keywords
+   * @param[in] HeterogeneousMap A map of strings to keys
+   */
+
+	std::cout<<"QuantumGEP::initialize\n";
+
+  if (!parameters.pointerLikeExists<Accelerator>("accelerator")) {
+    xacc::error("Acc was false");
+    return false;
+  }
+
+  if (!parameters.pointerLikeExists<Optimizer>("optimizer")) {
+    xacc::error("Opt was false");
+    return false;
+  }
+
+  optimizer = parameters.getPointerLike<Optimizer>("optimizer");
+  accelerator = parameters.getPointerLike<Accelerator>("accelerator");
+
+  return true;
+}
+
+const std::vector<std::string> QuantumGEP::requiredParameters() const {
+  return {"accelerator"};
+}
+
+void QuantumGEP::execute(const std::shared_ptr<AcceleratorBuffer> buffer) const {
+
+	std::cout<<"execute\n";
+  return;
+}
+
+} // namespace algorithm
+} // namespace xacc
---
+++ src/xacc/QuantumGEP_Plugin.hh
@@ -0,0 +1,36 @@
+#ifndef QUANTUM_GEP_PLUGIN_HH_
+#define QUANTUM_GEP_PLUGIN_HH_
+
+#include "Algorithm.hpp"
+#include "AlgorithmGradientStrategy.hpp"
+#include "xacc.hpp"
+#include "xacc_service.hpp"
+#include <chrono>
+#include <fstream>
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace xacc {
+
+namespace algorithm {
+
+class QuantumGEP : public Algorithm {
+
+protected:
+  Optimizer *optimizer;
+  Accelerator *accelerator;
+  HeterogeneousMap parameters;
+  std::shared_ptr<AlgorithmGradientStrategy> gradientStrategy;
+
+public:
+  bool initialize(const HeterogeneousMap &parameters) override;
+  const std::vector<std::string> requiredParameters() const override;
+  void execute(const std::shared_ptr<AcceleratorBuffer> buffer) const override;
+  const std::string name() const override { return "mc-vqe"; }
+  const std::string description() const override { return ""; }
+  DEFINE_ALGORITHM_CLONE(QuantumGEP)
+};
+} // namespace algorithm
+} // namespace xacc
+#endif
