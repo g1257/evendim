3608e634782085294a768f98d7c6cd7294c6e3a6
Gonzalo Alvarez <gz1@ornl.gov>
2023-11-14 23:18:10 -0500

    Working on XACC backend

--- src/xacc/obsolete/QuantumGEPXacc.cpp
+++ src/xacc/obsolete/QuantumGEPXacc.cpp
@@ -6,39 +6,37 @@
 
 int main(int argc, char* argv[])
 {
-    xacc::Initialize(argc, argv);
+	xacc::Initialize(argc, argv);
 
-    int exatnLogLevel = 2;
-    int n_states = 5;
+	int exatnLogLevel = 2;
+	int n_states = 5;
 
-    xacc::setLoggingLevel(exatnLogLevel);
+	xacc::setLoggingLevel(exatnLogLevel);
 
-    // path to data file
-    auto data_path = "@CMAKE_SOURCE_DIR@/examples/";
-    auto optimizer = xacc::getOptimizer("nlopt", { { "algorithm", "cobyla" },
-                                                  { "nlopt-maxeval", 10000 },
-                                                  { "nlopt-maxeval", 1000 } });
+	// path to data file
+	auto data_path = "@CMAKE_SOURCE_DIR@/examples/";
+	auto optimizer = xacc::getOptimizer("nlopt", { { "algorithm", "cobyla" }, { "nlopt-maxeval", 10000 }, { "nlopt-maxeval", 1000 } });
 
-    auto accelerator = xacc::getAccelerator("qpp");
+	auto accelerator = xacc::getAccelerator("qpp");
 
-    // get reference to Accelerator and Optimizer
-    // define data_path
-    auto n_chromophores = 4;
-    auto quantumGEP = xacc::getAlgorithm("QuantumGEP");
-    quantumGEP->initialize({ { "accelerator", accelerator },
-                            { "optimizer", optimizer },
-                            { "interference", true },
-                            { "n-states", n_states },
-                            { "data-path", data_path },
-                            { "cyclic", true },
-                            { "tnqvm-log", true },
-                            { "angstrom-to-au", true },
-                            { "debye-to-au", true },
-                            { "nChromophores", n_chromophores } });
+	// get reference to Accelerator and Optimizer
+	// define data_path
+	auto n_chromophores = 4;
+	auto quantumGEP = xacc::getAlgorithm("QuantumGEP");
+	quantumGEP->initialize({ { "accelerator", accelerator },
+	    { "optimizer", optimizer },
+	    { "interference", true },
+	    { "n-states", n_states },
+	    { "data-path", data_path },
+	    { "cyclic", true },
+	    { "tnqvm-log", true },
+	    { "angstrom-to-au", true },
+	    { "debye-to-au", true },
+	    { "nChromophores", n_chromophores } });
 
-    // allocate buffer and execute
-    auto buffer = xacc::qalloc(n_chromophores);
-    quantumGEP->execute(buffer);
+	// allocate buffer and execute
+	auto buffer = xacc::qalloc(n_chromophores);
+	quantumGEP->execute(buffer);
 
-    return 0;
+	return 0;
 }
--- src/xacc/obsolete/QuantumGEP_Plugin.hh
+++ src/xacc/obsolete/QuantumGEP_Plugin.hh
@@ -11,26 +11,31 @@
 #include <string>
 #include <vector>
 
-namespace xacc {
+namespace xacc
+{
 
-namespace algorithm {
+namespace algorithm
+{
 
-class QuantumGEP : public Algorithm {
+	class QuantumGEP : public Algorithm
+	{
 
-protected:
-  Optimizer *optimizer;
-  Accelerator *accelerator;
-  HeterogeneousMap parameters;
-  std::shared_ptr<AlgorithmGradientStrategy> gradientStrategy;
+	protected:
 
-public:
-  bool initialize(const HeterogeneousMap &parameters) override;
-  const std::vector<std::string> requiredParameters() const override;
-  void execute(const std::shared_ptr<AcceleratorBuffer> buffer) const override;
-  const std::string name() const override { return "mc-vqe"; }
-  const std::string description() const override { return ""; }
-  DEFINE_ALGORITHM_CLONE(QuantumGEP)
-};
+		Optimizer* optimizer;
+		Accelerator* accelerator;
+		HeterogeneousMap parameters;
+		std::shared_ptr<AlgorithmGradientStrategy> gradientStrategy;
+
+	public:
+
+		bool initialize(const HeterogeneousMap& parameters) override;
+		const std::vector<std::string> requiredParameters() const override;
+		void execute(const std::shared_ptr<AcceleratorBuffer> buffer) const override;
+		const std::string name() const override { return "mc-vqe"; }
+		const std::string description() const override { return ""; }
+		DEFINE_ALGORITHM_CLONE(QuantumGEP)
+	};
 } // namespace algorithm
 } // namespace xacc
 #endif
